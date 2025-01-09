6b9feee0f151799776bdb0bcc742c4c932895774
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2025-01-09 13:13:58 +0530

    new updates

--- src/Scroll.jsx
+++ src/Scroll.jsx
@@ -4,12 +4,14 @@ const Scroll = () => {
   return (
     <div>
         <div className='border-2 border-black/[0.1] relative top-60 left-32 rounded-xl h-36 transform hover:translate-y-10 hover:shadow-2xl bg-red-500'>
-        <div className=" border border-white/[0.2] rounded-2xl w-1/4 absolute left-36 top-8 hover:z-10 hover:translate-x-10 hover:translate-y-8 hover:shadow-2xl hover:shadow-black hover:rotate-12 ">
-            somethign is here 
-        </div>
+       
       </div>
     </div>
   )
 }
 
 export default Scroll
+
+//  <div className=" border border-white/[0.2] rounded-2xl w-1/4 absolute left-36 top-8 hover:z-10 hover:translate-x-10 hover:translate-y-8 hover:shadow-2xl hover:shadow-black hover:rotate-12 ">
+// somethign is here 
+// </div>
